8bf8cc0de40355fedad17d1c6b0c930d8bb7296e
Nguyen Pham <baonguyenyam@gmail.com>
2022-02-03 11:23:42 -0600

    fixed

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,192 @@
 "use strict";
 
+function _typeof3(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && _typeof3(Symbol.iterator) === "symbol") { _typeof3 = function _typeof(obj) { return typeof obj === "undefined" ? "undefined" : _typeof3(obj); }; } else { _typeof3 = function _typeof2(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj === "undefined" ? "undefined" : _typeof3(obj); }; } return _typeof3(obj); }
+
+/*! modernizr 3.6.0 (Custom Build) | MIT *
+ * https://modernizr.com/download/?-setclasses !*/
+!function (n, e, s) {
+  function o(n, e) {
+    return (typeof n === "undefined" ? "undefined" : _typeof3(n)) === e;
+  }
+
+  function a() {
+    var n, e, s, a, i, l, r;
+
+    for (var c in f) {
+      if (f.hasOwnProperty(c)) {
+        if (n = [], e = f[c], e.name && (n.push(e.name.toLowerCase()), e.options && e.options.aliases && e.options.aliases.length)) for (s = 0; s < e.options.aliases.length; s++) {
+          n.push(e.options.aliases[s].toLowerCase());
+        }
+
+        for (a = o(e.fn, "function") ? e.fn() : e.fn, i = 0; i < n.length; i++) {
+          l = n[i], r = l.split("."), 1 === r.length ? Modernizr[r[0]] = a : (!Modernizr[r[0]] || Modernizr[r[0]] instanceof Boolean || (Modernizr[r[0]] = new Boolean(Modernizr[r[0]])), Modernizr[r[0]][r[1]] = a), t.push((a ? "" : "no-") + r.join("-"));
+        }
+      }
+    }
+  }
+
+  function i(n) {
+    var e = r.className,
+        s = Modernizr._config.classPrefix || "";
+
+    if (c && (e = e.baseVal), Modernizr._config.enableJSClass) {
+      var o = new RegExp("(^|\\s)" + s + "no-js(\\s|$)");
+      e = e.replace(o, "$1" + s + "js$2");
+    }
+
+    Modernizr._config.enableClasses && (e += " " + s + n.join(" " + s), c ? r.className.baseVal = e : r.className = e);
+  }
+
+  var t = [],
+      f = [],
+      l = {
+    _version: "3.6.0",
+    _config: {
+      classPrefix: "",
+      enableClasses: !0,
+      enableJSClass: !0,
+      usePrefixes: !0
+    },
+    _q: [],
+    on: function on(n, e) {
+      var s = this;
+      setTimeout(function () {
+        e(s[n]);
+      }, 0);
+    },
+    addTest: function addTest(n, e, s) {
+      f.push({
+        name: n,
+        fn: e,
+        options: s
+      });
+    },
+    addAsyncTest: function addAsyncTest(n) {
+      f.push({
+        name: null,
+        fn: n
+      });
+    }
+  },
+      Modernizr = function Modernizr() {};
+
+  Modernizr.prototype = l, Modernizr = new Modernizr();
+  var r = e.documentElement,
+      c = "svg" === r.nodeName.toLowerCase();
+  a(), i(t), delete l.addTest, delete l.addAsyncTest;
+
+  for (var u = 0; u < Modernizr._q.length; u++) {
+    Modernizr._q[u]();
+  }
+
+  n.Modernizr = Modernizr;
+}(window, document);
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 20,
+      density: {
+        enable: true,
+        value_area: 200
+      }
+    },
+    color: {
+      value: ["#fff", "#fff", "#fff", "#fff"]
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#fff"
+      },
+      polygon: {
+        nb_sides: 15
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1.5,
+        opacity_min: 0.15,
+        sync: false
+      }
+    },
+    size: {
+      value: 2.5,
+      random: false,
+      anim: {
+        enable: true,
+        speed: 2,
+        size_min: 0.15,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 110,
+      color: "#fff",
+      opacity: 0.25,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 1.6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: false,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
+});
+
 function getParameterByName(name) {
   var url = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : window.location.href;
   name = name.replace(/[\[\]]/g, '\\$&');
