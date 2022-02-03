920cb4da7fcf53cf70b1f0a06033ad361edd0f8d
Nguyen Pham <baonguyenyam@gmail.com>
2022-02-03 15:17:25 -0600

    home bg

--- dist/js/app.js
+++ dist/js/app.js
@@ -221,24 +221,41 @@ function jsonLoad(path, success, error) {
 
 var WEILION_APP = {
   // Init App
-  init: function init() {}
+  loadingAnimation: function loadingAnimation() {
+    $('#weilion_loading').addClass('loading');
+    setTimeout(function () {
+      $('body').addClass('loaded');
+      $('#weilion_loading').removeClass('loading').addClass('loaded');
+      $('#weilion_loading').fadeOut(500);
+    }, 2000);
+  },
+  playAudio: function playAudio() {
+    if ($('#weilion_loading__audio')) {
+      $('#weilion_loading__audio').get(0).pause();
+      $('#weilion_loading__audio').get(0).play();
+    }
+  },
+  fixedHeader: function fixedHeader() {
+    var header = $('#weilion-header');
+    var headerHeight = header.outerHeight();
+    var scrollTop = $(window).scrollTop();
+
+    if (scrollTop > headerHeight) {
+      header.addClass('active');
+    } else {
+      header.removeClass('active');
+    }
+  },
+  init: function init() {
+    WEILION_APP.loadingAnimation();
+    WEILION_APP.playAudio();
+    WEILION_APP.fixedHeader();
+  }
 };
 jQuery(function () {
   WEILION_APP.init();
 });
-$('.btn_nav').click(function () {// animate content
-  // $('.page__style').addClass('animate_content');
-  // $('.page__description').fadeOut(100).delay(2800).fadeIn();
-  // setTimeout(function () {
-  // 	$('.page__style').removeClass('animate_content');
-  // }, 3200);
-  //remove fadeIn class after 1500ms
-  // setTimeout(function () {
-  // 	$('.page__style').removeClass('fadeIn');
-  // }, 1500);
-}); // $('.projects_link').click(function () {
-// 	setTimeout(function () {
-// 		$('.projects').addClass('fadeIn');
-// 	}, 1500);
-// });
+jQuery(window).on('scroll', function () {
+  WEILION_APP.fixedHeader();
+});
 //# sourceMappingURL=app.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,40 @@
 // APP
 var WEILION_APP = {
 	// Init App
+	loadingAnimation: () => {
+		$('#weilion_loading').addClass('loading');
+		setTimeout(() => {
+			$('body').addClass('loaded');
+			$('#weilion_loading').removeClass('loading').addClass('loaded');
+			$('#weilion_loading').fadeOut(500);
+		}, 2000);
+	},
+	playAudio: () => {
+		if ($('#weilion_loading__audio')) {
+			$('#weilion_loading__audio').get(0).pause();
+			$('#weilion_loading__audio').get(0).play();
+		}
+	},
+	fixedHeader: () => {
+		var header = $('#weilion-header');
+		var headerHeight = header.outerHeight();
+		var scrollTop = $(window).scrollTop();
+		if (scrollTop > headerHeight) {
+			header.addClass('active');
+		} else {
+			header.removeClass('active');
+		}
+	},
 	init: () => {
-
-
-
-
+		WEILION_APP.loadingAnimation();
+		WEILION_APP.playAudio();
+		WEILION_APP.fixedHeader();
 	},
 }
 
 jQuery(() => {
 	WEILION_APP.init();
 });
-
-
-$('.btn_nav').click(function () {
-	// animate content
-	// $('.page__style').addClass('animate_content');
-	// $('.page__description').fadeOut(100).delay(2800).fadeIn();
-
-	// setTimeout(function () {
-	// 	$('.page__style').removeClass('animate_content');
-	// }, 3200);
-
-	//remove fadeIn class after 1500ms
-	// setTimeout(function () {
-	// 	$('.page__style').removeClass('fadeIn');
-	// }, 1500);
-
+jQuery(window).on('scroll', function () {
+	WEILION_APP.fixedHeader();
 });
-
-// $('.projects_link').click(function () {
-// 	setTimeout(function () {
-// 		$('.projects').addClass('fadeIn');
-// 	}, 1500);
-// });
