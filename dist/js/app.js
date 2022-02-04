65ef00abf4e496677fdb0c7010dcfcb17fca4546
Nguyen Pham <baonguyenyam@gmail.com>
2022-02-03 20:18:55 -0600

    home page

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,191 +1,5 @@
 "use strict";
 
-function _typeof3(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && _typeof3(Symbol.iterator) === "symbol") { _typeof3 = function _typeof(obj) { return typeof obj === "undefined" ? "undefined" : _typeof3(obj); }; } else { _typeof3 = function _typeof2(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj === "undefined" ? "undefined" : _typeof3(obj); }; } return _typeof3(obj); }
-
-/*! modernizr 3.6.0 (Custom Build) | MIT *
- * https://modernizr.com/download/?-setclasses !*/
-!function (n, e, s) {
-  function o(n, e) {
-    return (typeof n === "undefined" ? "undefined" : _typeof3(n)) === e;
-  }
-
-  function a() {
-    var n, e, s, a, i, l, r;
-
-    for (var c in f) {
-      if (f.hasOwnProperty(c)) {
-        if (n = [], e = f[c], e.name && (n.push(e.name.toLowerCase()), e.options && e.options.aliases && e.options.aliases.length)) for (s = 0; s < e.options.aliases.length; s++) {
-          n.push(e.options.aliases[s].toLowerCase());
-        }
-
-        for (a = o(e.fn, "function") ? e.fn() : e.fn, i = 0; i < n.length; i++) {
-          l = n[i], r = l.split("."), 1 === r.length ? Modernizr[r[0]] = a : (!Modernizr[r[0]] || Modernizr[r[0]] instanceof Boolean || (Modernizr[r[0]] = new Boolean(Modernizr[r[0]])), Modernizr[r[0]][r[1]] = a), t.push((a ? "" : "no-") + r.join("-"));
-        }
-      }
-    }
-  }
-
-  function i(n) {
-    var e = r.className,
-        s = Modernizr._config.classPrefix || "";
-
-    if (c && (e = e.baseVal), Modernizr._config.enableJSClass) {
-      var o = new RegExp("(^|\\s)" + s + "no-js(\\s|$)");
-      e = e.replace(o, "$1" + s + "js$2");
-    }
-
-    Modernizr._config.enableClasses && (e += " " + s + n.join(" " + s), c ? r.className.baseVal = e : r.className = e);
-  }
-
-  var t = [],
-      f = [],
-      l = {
-    _version: "3.6.0",
-    _config: {
-      classPrefix: "",
-      enableClasses: !0,
-      enableJSClass: !0,
-      usePrefixes: !0
-    },
-    _q: [],
-    on: function on(n, e) {
-      var s = this;
-      setTimeout(function () {
-        e(s[n]);
-      }, 0);
-    },
-    addTest: function addTest(n, e, s) {
-      f.push({
-        name: n,
-        fn: e,
-        options: s
-      });
-    },
-    addAsyncTest: function addAsyncTest(n) {
-      f.push({
-        name: null,
-        fn: n
-      });
-    }
-  },
-      Modernizr = function Modernizr() {};
-
-  Modernizr.prototype = l, Modernizr = new Modernizr();
-  var r = e.documentElement,
-      c = "svg" === r.nodeName.toLowerCase();
-  a(), i(t), delete l.addTest, delete l.addAsyncTest;
-
-  for (var u = 0; u < Modernizr._q.length; u++) {
-    Modernizr._q[u]();
-  }
-
-  n.Modernizr = Modernizr;
-}(window, document); // particlesJS("particles-js", {
-// 	particles: {
-// 		number: {
-// 			value: 20,
-// 			density: {
-// 				enable: true,
-// 				value_area: 200,
-// 			},
-// 		},
-// 		color: {
-// 			value: ["#fff", "#fff", "#fff", "#fff"],
-// 		},
-// 		shape: {
-// 			type: "circle",
-// 			stroke: {
-// 				width: 0,
-// 				color: "#fff",
-// 			},
-// 			polygon: {
-// 				nb_sides: 15,
-// 			},
-// 		},
-// 		opacity: {
-// 			value: 0.5,
-// 			random: false,
-// 			anim: {
-// 				enable: false,
-// 				speed: 1.5,
-// 				opacity_min: 0.15,
-// 				sync: false,
-// 			},
-// 		},
-// 		size: {
-// 			value: 2.5,
-// 			random: false,
-// 			anim: {
-// 				enable: true,
-// 				speed: 2,
-// 				size_min: 0.15,
-// 				sync: false,
-// 			},
-// 		},
-// 		line_linked: {
-// 			enable: true,
-// 			distance: 110,
-// 			color: "#fff",
-// 			opacity: 0.25,
-// 			width: 1,
-// 		},
-// 		move: {
-// 			enable: true,
-// 			speed: 1.6,
-// 			direction: "none",
-// 			random: false,
-// 			straight: false,
-// 			out_mode: "out",
-// 			bounce: false,
-// 			attract: {
-// 				enable: false,
-// 				rotateX: 600,
-// 				rotateY: 1200,
-// 			},
-// 		},
-// 	},
-// 	interactivity: {
-// 		detect_on: "canvas",
-// 		events: {
-// 			onhover: {
-// 				enable: false,
-// 				mode: "repulse",
-// 			},
-// 			onclick: {
-// 				enable: false,
-// 				mode: "push",
-// 			},
-// 			resize: true,
-// 		},
-// 		modes: {
-// 			grab: {
-// 				distance: 400,
-// 				line_linked: {
-// 					opacity: 1,
-// 				},
-// 			},
-// 			bubble: {
-// 				distance: 400,
-// 				size: 40,
-// 				duration: 2,
-// 				opacity: 8,
-// 				speed: 3,
-// 			},
-// 			repulse: {
-// 				distance: 200,
-// 				duration: 0.4,
-// 			},
-// 			push: {
-// 				particles_nb: 4,
-// 			},
-// 			remove: {
-// 				particles_nb: 2,
-// 			},
-// 		},
-// 	},
-// 	retina_detect: true,
-// });
-
 function getParameterByName(name) {
   var url = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : window.location.href;
   name = name.replace(/[\[\]]/g, '\\$&');
@@ -246,10 +60,51 @@ var WEILION_APP = {
       header.removeClass('active');
     }
   },
+  toggleMenu: function toggleMenu() {
+    var welionMenu = $('#welionToggleMenu').width();
+    $('#welionMenu').on('click', function () {
+      $('#welionMenu, #welionToggleMenu').toggleClass('open');
+
+      if ($(this).attr('data-click-state') == 1) {
+        $(this).attr('data-click-state', 0);
+        $('#welionToggleMenu').css({
+          "width": "0px"
+        });
+      } else {
+        $(this).attr('data-click-state', 1);
+        $('#welionToggleMenu').css({
+          "width": welionMenu + "px"
+        });
+      }
+    });
+    setTimeout(function () {
+      $('#welionToggleMenu').css({
+        "width": "0px"
+      });
+    }, 500);
+  },
+  indexText: function indexText() {
+    var typed = new Typed('#text-replace', {
+      strings: ["<span>First stage – S-IC</span><span>Length 138.0 ft (42.1 m) </span><br><span>Diameter 33.0 ft (10.1 m) </span><br><span>Empty mass 287,000 lb (130,000 kg) </span><br><span>Gross mass 5,040,000 lb (2,290,000 kg) </span><br><span>Engines 5 Rocketdyne F-1 </span><br><span>Thrust 7,891,000 lbf (35,100 kN) sea level </span><br><span>Specific impulse 263 seconds (2.58 km/s) sea level</span>"],
+      typeSpeed: 20,
+      startDelay: 1000,
+      backDelay: 10000,
+      loop: true,
+      contentType: 'html',
+      fadeOut: true,
+      fadeOutClass: 'typed-fade-out',
+      fadeOutDelay: 500,
+      onTypingResumed: function onTypingResumed(self) {
+        console.log('last string backspaced!');
+      }
+    });
+  },
   init: function init() {
     WEILION_APP.loadingAnimation();
     WEILION_APP.playAudio();
     WEILION_APP.fixedHeader();
+    WEILION_APP.toggleMenu();
+    WEILION_APP.indexText();
   }
 };
 jQuery(function () {
@@ -257,5 +112,8 @@ jQuery(function () {
 });
 jQuery(window).on('scroll', function () {
   WEILION_APP.fixedHeader();
+});
+jQuery(window).on('resize', function () {
+  WEILION_APP.fixedHeader(); // WEILION_APP.toggleMenu();
 });
 //# sourceMappingURL=app.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,16 +25,61 @@ var WEILION_APP = {
 			header.removeClass('active');
 		}
 	},
+	toggleMenu: () => {
+		let welionMenu = $('#welionToggleMenu').width();
+		$('#welionMenu').on('click', function () {
+			$('#welionMenu, #welionToggleMenu').toggleClass('open');
+			if ($(this).attr('data-click-state') == 1) {
+				$(this).attr('data-click-state', 0);
+				$('#welionToggleMenu').css({
+					"width": "0px",
+				});
+			}
+			else {
+				$(this).attr('data-click-state', 1);
+				$('#welionToggleMenu').css({
+					"width": welionMenu+"px",
+				});
+			}
+		});
+		setTimeout(() => {
+			$('#welionToggleMenu').css({
+				"width": "0px",
+			});
+		}, 500);
+	},
+	indexText: () => {
+		var typed = new Typed('#text-replace', {
+			strings: ["<span>First stage – S-IC</span><span>Length 138.0 ft (42.1 m) </span><br><span>Diameter 33.0 ft (10.1 m) </span><br><span>Empty mass 287,000 lb (130,000 kg) </span><br><span>Gross mass 5,040,000 lb (2,290,000 kg) </span><br><span>Engines 5 Rocketdyne F-1 </span><br><span>Thrust 7,891,000 lbf (35,100 kN) sea level </span><br><span>Specific impulse 263 seconds (2.58 km/s) sea level</span>"],
+			typeSpeed: 20,
+			startDelay: 1000,
+			backDelay: 10000,
+			loop: true,
+			contentType: 'html',
+			fadeOut: true,
+			fadeOutClass: 'typed-fade-out',
+			fadeOutDelay: 500,
+			onTypingResumed: (self) => {
+				console.log('last string backspaced!');
+			},
+		});
+	},
 	init: () => {
 		WEILION_APP.loadingAnimation();
 		WEILION_APP.playAudio();
 		WEILION_APP.fixedHeader();
+		WEILION_APP.toggleMenu();
+		WEILION_APP.indexText();
 	},
 }
 
 jQuery(() => {
 	WEILION_APP.init();
 });
-jQuery(window).on('scroll', function () {
+jQuery(window).on('scroll', () => {
 	WEILION_APP.fixedHeader();
+});
+jQuery(window).on('resize', () => {
+	WEILION_APP.fixedHeader();
+	// WEILION_APP.toggleMenu();
 });
