e60e7aa42ea0fa3a853b43ed824ac9ce60ff4505
Nguyen Pham <baonguyenyam@gmail.com>
2022-02-06 11:55:59 -0600

    fix

--- tasks/concat-css.js
+++ tasks/concat-css.js
@@ -1,7 +1,7 @@
 module.exports = function (gulp, $, browserSync) {
 	gulp.task('concat-css', function () {
 		return gulp.src([
-				'bower_components/font-awesome-5/css/all.min.css',
+				// 'bower_components/font-awesome-5/css/all.min.css',
 				// 'bower_components/font-awesome/css/font-awesome.min.css',
 				// 'bower_components/animate.css/animate.min.css',
 				// OWL
