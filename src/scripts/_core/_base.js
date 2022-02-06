0e884e48fb0abaa065985360d87d03e87277dbc9
Nguyen Pham <baonguyenyam@gmail.com>
2022-02-06 12:22:26 -0600

    team

--- src/scripts/_core/_base.js
+++ src/scripts/_core/_base.js
@@ -99,77 +99,35 @@ if ($(window).width() < 768) {
         offset: 0,
     });
 }
-// function debounce(func, wait, immediate) {
-//     var timeout;
-//     return function () {
-//         var context = this,
-//             args = arguments;
-//         var later = function () {
-//             timeout = null;
-//             if (!immediate) {
-//                 func.apply(context, args);
-//             }
-//         };
-//         var callNow = immediate && !timeout;
-//         clearTimeout(timeout);
-//         timeout = setTimeout(later, wait);
-//         if (callNow) {
-//             func.apply(context, args);
-//         }
-//     };
-// }
 
-// var htmlTag = document.getElementsByTagName('html')[0];
-// var videoContainer = document.querySelector('#video-container');
-// var videoElem = document.querySelector('#video-container video');
+// Mouse Animation
+document.onmousemove = animateCircles; // circle follow mouse
+var colors = ['#f9f9f9', '#04b8ff', , '#000', '#f26528']
 
-// var minW = 320; // Minimum video width allowed
-// var vidWOrig; // Original video dimensions
-// var vidHOrig;
+function animateCircles(event) {
+    var circle = document.createElement("div");
+    circle.setAttribute("class", "weilion-mouse-circle");
+    document.body.appendChild(circle); // adds function to body
 
-// vidWOrig = videoElem.getAttribute('width');
-// vidHOrig = videoElem.getAttribute('height');
+    // adds motion
+    circle.style.left = event.clientX + 'px';
+    circle.style.top = event.clientY + 'px';
 
-// var videoCover = function () {
+    // randomize color
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    circle.style.borderColor = color;
 
-//     var winWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-//     var winHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    // adds animation
+    circle.style.transition = "all 0.5s linear 0s";
 
-//     // Set the video viewport to the window size
+    circle.style.left = circle.offsetLeft - 20 + 'px';
+    circle.style.top = circle.offsetTop - 20 + 'px';
 
-//     videoContainer.style.width = winWidth + 'px';
-//     videoContainer.style.height = winHeight + 'px';
-
-//     // Use largest scale factor of horizontal/vertical
-//     var scaleH = winWidth / vidWOrig;
-//     var scaleV = winHeight / vidHOrig;
-//     var scale = scaleH > scaleV ? scaleH : scaleV;
-
-//     // Don't allow scaled width < minimum video width
-//     if (scale * vidWOrig < minW) {
-//         scale = minW / vidWOrig;
-//     }
-
-//     // Scale the video
-//     var videoNewWidth = scale * vidWOrig;
-//     var videoNewHeight = scale * vidHOrig;
-
-//     videoElem.style.width = videoNewWidth + 'px';
-//     videoElem.style.height = videoNewHeight + 'px';
-
-//     // Center it by scrolling the video viewport
-//     videoContainer.scrollLeft = (videoNewWidth - winWidth) / 2;
-//     videoContainer.scrollTop = (videoNewHeight - winHeight) / 2;
-
-// };
-
-// if (htmlTag.classList.contains('no-touch')) {
-//     videoCover();
-
-//     // Adjust on resize
-//     var updateVideo = debounce(function () {
-//         videoCover();
-//     }, 100);
-
-//     window.addEventListener('resize', updateVideo);
-// }
+    circle.style.width = "50px";
+    circle.style.height = "50px";
+    circle.style.borderWidth = "5px";
+    circle.style.opacity = 0;
+    setTimeout(() => {
+        circle.remove()
+    }, 500);
+}
